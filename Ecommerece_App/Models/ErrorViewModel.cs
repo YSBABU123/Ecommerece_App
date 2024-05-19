2c8069166c841ea191260a14d50b1edf39bffb70
suresh babu <yanakisuresh783@gmail.com>
2024-05-19 18:27:30 +0530

    Add project files.

---
+++ Ecommerece_App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecommerece_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
